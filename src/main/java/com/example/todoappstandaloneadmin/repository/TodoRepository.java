2230d4a22e9168176db1acc853bce5ced36f8cb7
linushakansson <linus.hakansson@skolon.com>
2023-12-11 11:25:38 +0100

    cleaning up the code, removed unused imports etc.

--- src/main/java/com/example/todoappstandaloneadmin/repository/TodoRepository.java
+++ src/main/java/com/example/todoappstandaloneadmin/repository/TodoRepository.java
@@ -2,9 +2,7 @@ package com.example.todoappstandaloneadmin.repository;
 
 import com.example.todoappstandaloneadmin.entity.TodoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.List;
 public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
 
 }
