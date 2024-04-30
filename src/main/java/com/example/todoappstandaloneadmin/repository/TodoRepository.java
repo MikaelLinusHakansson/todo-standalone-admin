a1df548c100f5562caf13aaf5148402d3b3766b9
linush <linus_hakansson@outlook.com>
2024-04-30 10:25:50 +0200

    Fixed some OCD stuff with whitespaces.

--- src/main/java/com/example/todoappstandaloneadmin/repository/TodoRepository.java
+++ src/main/java/com/example/todoappstandaloneadmin/repository/TodoRepository.java
@@ -1,7 +1,8 @@
 package com.example.todoappstandaloneadmin.repository;
 
-import com.example.todoappstandaloneadmin.entity.TodoEntity;
 import org.springframework.data.repository.CrudRepository;
+
+import com.example.todoappstandaloneadmin.entity.TodoEntity;
 
 import java.util.List;
 
