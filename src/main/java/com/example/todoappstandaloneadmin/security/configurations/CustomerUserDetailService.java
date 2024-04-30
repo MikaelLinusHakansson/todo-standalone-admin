47669135357fe34053e91f573df2f5d6d40c61a0
linush <linus_hakansson@outlook.com>
2024-04-30 10:24:05 +0200

    ocd changes. and also added final clause to the repository. Fixed some minor ocd change in authenticationFilter too.

--- src/main/java/com/example/todoappstandaloneadmin/security/configurations/CustomerUserDetailService.java
+++ src/main/java/com/example/todoappstandaloneadmin/security/configurations/CustomerUserDetailService.java
@@ -1,8 +1,5 @@
 package com.example.todoappstandaloneadmin.security.configurations;
 
-import com.example.todoappstandaloneadmin.entity.UserEntity;
-import com.example.todoappstandaloneadmin.entity.UserRoles;
-import com.example.todoappstandaloneadmin.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,8 +7,11 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import com.example.todoappstandaloneadmin.entity.UserEntity;
+import com.example.todoappstandaloneadmin.entity.UserRoles;
+import com.example.todoappstandaloneadmin.repository.UserRepository;
 
 import java.util.Collection;
 import java.util.List;
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class CustomerUserDetailService implements UserDetailsService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public CustomerUserDetailService(UserRepository userRepository) {
@@ -34,8 +34,8 @@ public class CustomerUserDetailService implements UserDetailsService {
      */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException("Username not found"));
+        UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
         return new User(userEntity.getUsername(), userEntity.getPassword(), mapRolesToAuthorities(userEntity.getRoles()));
     }
 
